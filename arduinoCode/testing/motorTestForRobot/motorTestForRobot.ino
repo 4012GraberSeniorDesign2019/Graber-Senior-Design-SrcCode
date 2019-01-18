f7e890bc61e20a916e1b8031a0b9e9ef01e29296
Nelson R <NelsonRaphael27@gmail.com>
2019-01-18 13:36:25 -0500

    Adding testing code for manipulating LEDs and motors

---
+++ arduinoCode/testing/motorTestForRobot/motorTestForRobot.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+//pins to choose 2,3,4,5, 8,9,10,11
+
+
+analogWrite(2,23.5);
+}
