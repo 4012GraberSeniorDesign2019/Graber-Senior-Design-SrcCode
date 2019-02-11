34ce6405908fd312bacbad18c44dd726a392b6e2
Nelson R <NelsonRaphael27@gmail.com>
2019-02-11 18:55:46 -0500

    edits for the broker

--- pcSide/comm/pcSideCommunication.py
+++ pcSide/comm/pcSideCommunication.py
@@ -1,12 +1,17 @@
 import paho.mqtt.client as mqtt
 import time
 
+broker = "143.215.102.14"
+port = 1883
+topic = "test"
+
+
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, rc):
     print("Connected with result code "+str(rc))
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("hello")
+    client.subscribe(topic)
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
@@ -16,10 +21,10 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 for i in range(0,10):
-    client.publish("hello","Boo Back")
+    client.publish(topic,"Boo Back")
     time.sleep(1)
 
-client.connect("iot.eclipse.org", 1883)
+client.connect(broker, port)
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
