6c3b8771c6b113c01e377738cf9fc769c8e9adaa
Nels27 <NelsonRaphael27@gmail.com>
2019-03-25 17:28:54 -0400

    adding demo scripts for testing serial connection between due and pi

---
+++ serialDemos/serialTest.py
@@ -0,0 +1,22 @@
+import serial
+import time
+
+ser = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=2)
+message = "thiss is crazy"
+print(ser.in_waiting)
+while True:
+    while (ser.in_waiting>0):
+        print("in: "+str(ser.in_waiting))
+        ser.reset_input_buffer()
+        time.sleep(.01)
+    while (ser.out_waiting>0):
+        print("out: "+str(ser.out_waiting))
+        ser.reset_output_buffer()
+        time.sleep(.01)
+    ser.write(message.encode())
+    print("message sent")
+    data = ser.readline()
+    if data or True:
+        print(data.decode())
+    else:
+        print('nothing')
