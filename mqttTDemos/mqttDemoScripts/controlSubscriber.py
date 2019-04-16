ba2b171110bd0f10632359acb07c9f7e66ec4f86
Nelson R <NelsonRaphael27@gmail.com>
2019-04-16 00:15:31 -0400

    adjustments from Nelson terminal

--- mqttTDemos/mqttDemoScripts/controlSubscriber.py
+++ mqttTDemos/mqttDemoScripts/controlSubscriber.py
@@ -56,21 +56,21 @@ class controlSubscriber(object):
         #on the Arduino DUE
 
         print('wrote to DUE')
-        #ser = serial.Serial(port='COM8', baudrate=9600, timeout=2)
+        ser = serial.Serial(port='COM8', baudrate=9600, timeout=2)
 
-        #while True:
-        #    while (ser.in_waiting>0):
-        #        print("in: "+str(ser.in_waiting))
-        #        ser.reset_input_buffer()
-        #        time.sleep(.01)
-        #    while (ser.out_waiting>0):
-        #        print("out: "+str(ser.out_waiting))
-        #        ser.reset_output_buffer()
-        #        time.sleep(.01)
-        #    ser.write(message.encode())
-        #    print("message sent")
-        #    data = ser.readline()
-        #    if data or True:
-        #        print(data.decode())
-        #    else:
-        #        print('nothing')
+        while True:
+            while (ser.in_waiting>0):
+                print("in: "+str(ser.in_waiting))
+                ser.reset_input_buffer()
+                time.sleep(.01)
+            while (ser.out_waiting>0):
+                print("out: "+str(ser.out_waiting))
+                ser.reset_output_buffer()
+                time.sleep(.01)
+            ser.write(message.encode())
+            print("message sent")
+            data = ser.readline()
+            if data or True:
+                print(data.decode())
+            else:
+                print('nothing')
